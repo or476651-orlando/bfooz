de2b53ad1aa8bda18ba0985b518ef50ac7749c81
or476651-orlando <or476651@uaeh.edu.mx>
2026-02-20 00:06:49 +0000

    Agregar funciones para verificar coloración y ciclos hamiltonianos en grafos

--- bfooz/__init__.py
+++ bfooz/__init__.py
@@ -1,2 +1,41 @@
+import itertools
+import networkx as nx
+
 def hello():
     return "Hello, world!"
+
+def is_proper_coloring(graph, coloring):
+  for edge in graph.edges():
+    if coloring[edge[0]] == coloring[edge[1]]:
+      return False
+  return True
+
+def is_3_colorable(graph):
+  n = graph.order()
+  colorings = itertools.product([0,1,2], repeat = n)
+  for coloring in colorings:
+    if is_proper_coloring(graph, coloring):
+      return coloring
+  return False
+
+def is_hamiltonian_cycle(graph,cycle):
+  """ Checks if cycle is a hamiltonian cycle in graph """
+  n = len(set(cycle))
+  if n != graph.order():
+    return False
+  for i in range(n-1):
+    if not graph.has_edge(cycle[i],cycle[i+1]):
+      return False
+  if not graph.has_edge(cycle[n-1],cycle[0]):
+    return False
+  return True
+
+def is_hamiltonian(graph):
+  vertices = list(graph.nodes())
+  if len(vertices) < 3:
+    return False
+  perms = itertools.permutations(vertices)
+  for perm in perms:
+    if is_hamiltonian_cycle(graph,perm):
+      return perm
+  return False 
