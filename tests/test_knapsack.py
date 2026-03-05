6ec17f1fe01910c75c0333359a52d93584cb0d98
or476651-orlando <or476651@uaeh.edu.mx>
2026-03-05 23:11:00 +0000

    Actualizar pruebas para la función knapsack_boolean con nuevos casos de prueba

--- tests/test_knapsack.py
+++ tests/test_knapsack.py
@@ -1,8 +1,6 @@
 from bfooz import knapsack_boolean, knapsack
 
 def test_knapsack():
-    w = [2, 1, 3, 2]
-    p = [3, 2, 4, 2]
-    capacity = 5
-    goal = 7
-    assert knapsack_boolean.knapsack_boolean(goal, capacity, p, w) == True
+    assert knapsack_boolean(10, 10, [1,2,1,3], [1,2,3,4]) == False
+    assert knapsack_boolean(6, 4, [1,2,3], [1,2,3]) == False
+    assert knapsack_boolean(7, 5, [3, 2, 4, 2], [2, 1, 3, 2]) == True
